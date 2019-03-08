87df65341f8ace727178268e6be77c83a19966f4
matze10000 <mguett@googlemail.com>
2019-03-08 11:36:26 +0100

    React Native new requirements

--- android/src/main/java/com/reactlibrary/RNSimpleCompassPackage.java
+++ android/src/main/java/com/reactlibrary/RNSimpleCompassPackage.java
@@ -10,13 +10,14 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
+
 public class RNSimpleCompassPackage implements ReactPackage {
-    @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
       return Arrays.<NativeModule>asList(new RNSimpleCompassModule(reactContext));
     }
-
-    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+      return Collections.emptyList();
+    }
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Collections.emptyList();
     }
